0939bd330ea185046e51eadb50bd4132e6724ee6
trantuankiet123 <trantuankiet11884@gmail.com>
2023-07-21 11:49:02 +0700

    final

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import "./global.css";
 import Dashboard from "./pages/dashboard/dashboard";
 import LoginPage from "./pages/auth/Login";
-import { Route, Routes } from "react-router-dom";
+import { Navigate, Route, Routes, useNavigate } from "react-router-dom";
 import ConfirmForgotPwd from "./pages/auth/ForgotPwd";
 import Device from "./pages/devices/Device";
 import Profile from "./pages/profile/Profile";
@@ -12,9 +12,7 @@ import UpdateDevice from "./pages/devices/UpdateDevice";
 import Service from "./pages/services/Service";
 import PostService from "./pages/services/PostService";
 import DetailService from "./pages/services/DetailService";
-import { BrowserRouter } from "react-router-dom";
 import { Layout } from "antd";
-import { SiderBar } from "./components/Sidebar";
 import UpdateService from "./pages/services/UpdateService";
 import CapSo from "./pages/levelmanagment/Capso";
 import CapSoMoi from "./pages/levelmanagment/CapSoMoi";
@@ -38,6 +36,12 @@ function App() {
   const isLoggedIn = useSelector(
     (state: RootState) => state.account.isLoggedIn
   );
+  const navigate = useNavigate();
+  // useEffect(() => {
+  //   if (isLoggedIn === false) {
+  //     return navigate("/");
+  //   }
+  // }, [isLoggedIn]);
 
   useEffect(() => {
     const isLoggedInStr = localStorage.getItem("isLoggedIn");
@@ -47,53 +51,45 @@ function App() {
       const currentAccount = JSON.parse(currentAccountStr);
       dispatch(login(currentAccount));
     }
-  }, [dispatch]);
+  }, [dispatch, navigate]);
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Layout style={{ minHeight: "100vh" }}>
-          <Routes>
-            {isLoggedIn ? (
-              <>
-                <Route path="/dashboard" element={<Dashboard />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/devices" element={<Device />} />
-                <Route path="/post-device" element={<PostDevice />} />
-                <Route path="/details-device/:id" element={<DetailDevice />} />
-                <Route path="/update-device/:id" element={<UpdateDevice />} />
-                <Route path="/services" element={<Service />} />
-                <Route path="/post-service" element={<PostService />} />
-                <Route
-                  path="/details-service/:id"
-                  element={<DetailService />}
-                />
-                <Route path="/update-service/:id" element={<UpdateService />} />
-                <Route path="/numbers" element={<CapSo />} />
-                <Route path="/post-number" element={<CapSoMoi />} />
-                <Route path="/details-number/:id" element={<CapSoChiTiet />} />
-                <Route path="/reports" element={<Reportt />} />
-                <Route path="/roles" element={<RolePage />} />
-                <Route path="/post-role" element={<PostRole />} />
-                <Route path="/update-role/:id" element={<UpdateRole />} />
-                <Route path="/accountlist" element={<AccountPage />} />
-                <Route path="/post-account" element={<PostAccount />} />
-                <Route path="/update-account/:id" element={<UpdateAccount />} />
-                <Route path="/users" element={<NoteUser />} />
-              </>
-            ) : (
-              <>
-                <Route path="/" element={<LoginPage />} />
-                <Route
-                  path="/confirm-forgotpwd"
-                  element={<ConfirmForgotPwd />}
-                />
-                <Route path="/reset-pwd" element={<ResetPwd />} />
-              </>
-            )}
-          </Routes>
-        </Layout>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Layout style={{ minHeight: "100vh" }}>
+        <Routes>
+          {isLoggedIn ? (
+            <>
+              <Route path="/dashboard" element={<Dashboard />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="/devices" element={<Device />} />
+              <Route path="/post-device" element={<PostDevice />} />
+              <Route path="/details-device/:id" element={<DetailDevice />} />
+              <Route path="/update-device/:id" element={<UpdateDevice />} />
+              <Route path="/services" element={<Service />} />
+              <Route path="/post-service" element={<PostService />} />
+              <Route path="/details-service/:id" element={<DetailService />} />
+              <Route path="/update-service/:id" element={<UpdateService />} />
+              <Route path="/numbers" element={<CapSo />} />
+              <Route path="/post-number" element={<CapSoMoi />} />
+              <Route path="/details-number/:id" element={<CapSoChiTiet />} />
+              <Route path="/reports" element={<Reportt />} />
+              <Route path="/roles" element={<RolePage />} />
+              <Route path="/post-role" element={<PostRole />} />
+              <Route path="/update-role/:id" element={<UpdateRole />} />
+              <Route path="/accountlist" element={<AccountPage />} />
+              <Route path="/post-account" element={<PostAccount />} />
+              <Route path="/update-account/:id" element={<UpdateAccount />} />
+              <Route path="/users" element={<NoteUser />} />
+            </>
+          ) : (
+            <>
+              <Route path="/" element={<LoginPage />} />
+              <Route path="/confirm-forgotpwd" element={<ConfirmForgotPwd />} />
+              <Route path="/reset-pwd" element={<ResetPwd />} />
+            </>
+          )}
+        </Routes>
+      </Layout>
+    </div>
   );
 }
 
